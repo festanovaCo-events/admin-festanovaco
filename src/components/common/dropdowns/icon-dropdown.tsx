881ea2a9a7d2d784f7fa94de68134627ea7f76ad
dylanbatar-hf <dylan.batista@holafly.com>
2025-10-31 18:51:07 -0500

    feat: add IconDropdown component for enhanced dropdown functionality

---
+++ src/components/common/dropdowns/icon-dropdown.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/shadcn/ui/dropdown-menu";
+import { IconDropdownProps } from "@/interfaces";
+
+export const IconDropdown: React.FC<IconDropdownProps> = ({
+  options,
+  onSelect,
+  iconSize = 30,
+  children,
+}) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {options.map((option) => (
+          <DropdownMenuItem key={option.value} onClick={() => onSelect(option)}>
+            <Image
+              src={option.src}
+              alt={option.label}
+              width={iconSize}
+              height={iconSize}
+              draggable={false}
+            />
+            <span className="ml-2 text-sm font-normal">{option.label}</span>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
